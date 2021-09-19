5cb48c4ada67cc5ec36ba6de0434e1dab5577d7e
Roman Shershnev <RVShershnev@yandex.ru>
2021-09-19 06:56:30 +0300

    Add comments

--- Shershnev.Lab2.Exercise1/Program.cs
+++ Shershnev.Lab2.Exercise1/Program.cs
@@ -7,16 +7,18 @@ namespace Shershnev.Lab2.Exercise1
         static void Main(string[] args)
         {
             // Дана сторона квадрата, найти площадь, периметр и длину диагоналей.
-
             Console.WriteLine("Введите сторону квадрата");
             double a = Convert.ToDouble(Console.ReadLine());
 
+            // Площадь.
             double S = a * a;
             Console.WriteLine("площадь: " + S);
 
+            // Периметр.
             double P = 4 * a;
-            Console.WriteLine("периметр: " + P);      
+            Console.WriteLine("периметр: " + P);
 
+            // Диагональ.
             double G = Math.Sqrt(2 * a * a);
             Console.WriteLine("диагональ: " + G);
         }
